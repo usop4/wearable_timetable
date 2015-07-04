5d761dd8bfad911d8da8879c43761ca1c619de92
usopyon <t.uehara@gmail.com>
2015-07-04 23:04:46 +0900

    LEDを3から5に増やし、背景色を設定

--- wearable_timetable.ino
+++ wearable_timetable.ino
@@ -2,6 +2,7 @@
 #include <avr/power.h>
 
 // set time
+// 開始時間を設定
 
 unsigned long start_hour = 9UL * 1000UL * 60UL * 60UL;
 unsigned long start_min = 9UL * 1000UL * 60UL;
@@ -9,7 +10,7 @@ unsigned long start_min = 9UL * 1000UL * 60UL;
 // NeoPixel
 
 int pin = 3; //GEMMA->1,LilyPad->3
-int numpixels = 3;
+int numpixels = 5;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(numpixels, pin, NEO_GRB + NEO_KHZ800);
 
 // GEMMAに接続するときは0にする
@@ -17,11 +18,11 @@ int serial = 1;
 
 // デバッグ（sp=60ULにすると60倍速になる）
 int debug = 1;
-unsigned int sp = 60UL;
+unsigned int sp = 1UL;
 
 // time table
 // http://www.twr.co.jp/route/kokusai/timetable.html
-// ToDo:正しくは２駅前の東雲を入力。後で直す
+// ToDo:正しくは２駅前の新木場を入力。後で直す
 
 int table[28][9] = {
   {},//0
@@ -57,7 +58,7 @@ int s = 0;
 int m_old = 0;
 int s_old = 0;
 
-int delayval = 300;
+int delayval = 30;
 
 void setup() {
 
@@ -67,13 +68,6 @@ void setup() {
 
   pixels.begin(); // This initializes the NeoPixel library.
 
-  pixels.setPixelColor(0, pixels.Color(0,0,0));
-  pixels.show();
-  pixels.setPixelColor(1, pixels.Color(0,0,0));
-  pixels.show();
-  pixels.setPixelColor(2, pixels.Color(0,0,0));
-  pixels.show();
-
   if( serial == 1 ){
     Serial.begin(9600);    
   }
@@ -82,18 +76,21 @@ void setup() {
 
 void loop() {
 
-  pixels.setPixelColor(0, pixels.Color(0,0,0));
-  pixels.show();
-  pixels.setPixelColor(1, pixels.Color(0,0,0));
-  pixels.show();
-  pixels.setPixelColor(2, pixels.Color(0,0,0));
-  pixels.show();
-  
-  t = millis() + t_ini;
-
   h = (int)( t / (1000UL * 60UL * 60UL ) % 24UL );
   m = (int)( t / (1000UL * 60UL /sp) % 60UL );
   s = (int)( t / (1000UL /sp) % 60UL );
+
+  for( int i = 0; i < numpixels; i++ ){
+    if( s % 5 == i ){
+      pixels.setPixelColor( i, pixels.Color(0,0,5));
+      pixels.show();
+    }else{
+      pixels.setPixelColor( i, pixels.Color(0,0,0));
+      pixels.show();      
+    }
+  }
+  
+  t = millis() + t_ini;
   
   if( m != m_old ){
     if( serial == 1 ){
@@ -111,6 +108,8 @@ void loop() {
   check_table2(h,m,0,0);//当駅
   check_table2(h,m,1,2);//＋１駅（２分後）
   check_table2(h,m,2,4);//＋２駅（４分後）
+  check_table2(h,m,3,6);//＋３駅（６分後）
+  check_table2(h,m,4,8);//＋４駅（８分後）
 
   delay(delayval);
 }
@@ -118,7 +117,7 @@ void loop() {
 void check_table2(int h, int m, int p, int d){
   for(int i = 0; i < 9; i++ ){
     if( table[h][i]+d == m ){
-      pixels.setPixelColor(p, pixels.Color(100,100,100));
+      pixels.setPixelColor(p, pixels.Color(10,0,0));
       pixels.show();
       if( serial == 1 ){
         Serial.print("[");        
